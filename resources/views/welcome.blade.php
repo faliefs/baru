633e9f0b25b2caf6ad618fa3e71523a00d3c2adb
faliefs <faliefs@365.telkomuniversity.ac.id>
2016-05-25 15:25:47 +0700

    dari awal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,11 +7,6 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Welcome</div>
                 	TES DONG
-<<<<<<< HEAD
-                	hahahaha
-=======
-                    Coba Lagi..!!
->>>>>>> b2ec2b0be265f43b91a240e830e9d9def4e9afd9
                 <div class="panel-body">
                 </div>
             </div>
