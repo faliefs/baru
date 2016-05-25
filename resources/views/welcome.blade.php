d7461cf4f34752e1e0c4e2f79b08c0de31d7edaa
faliefs <faliefs@365.telkomuniversity.ac.id>
2016-05-25 14:39:54 +0700

    baru lagi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">Welcome</div>
-
+                	TES DONG
                 <div class="panel-body">
                 </div>
             </div>
