a2264f6a417b4e41a0c4b8fb5f0f5cd05885c0e1
faliefs <faliefs@365.telkomuniversity.ac.id>
2016-06-09 13:49:50 +0700

    userTest

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('home', 'HomeController@index');
+});
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -7,11 +7,15 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class UserTest extends TestCase
 {
+    use DatabaseTransactions;
     public function testExample(){
-        $user = factory(App\User::class)->create();
-        $this->actingAs($user)
-             ->withSession(['foo' => 'bar'])
-             ->visit('/')
-             ->see($user->name);
+        $user = factory(App\User::class)->create([
+            'name' => 'agung',
+            'email' => 'agung@tes.com',
+            'password' => 'agung123',
+        ]);
+        $this->actingAs($user);
+        $this->action('GET', 'HomeController@index');
+        $response = $this->call('GET', '/home');
     }
 }
